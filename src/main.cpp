498421585421b3a2f17e8ee6297990adfcb25dfb
yosmont <yosmont@users.noreply.github.com>
2024-11-26 14:35:20 +0100

    Config cli

--- src/main.cpp
+++ src/main.cpp
@@ -13,24 +13,17 @@ int main() {
 
   bot.on_log(dpp::utility::cout_logger());
 
-  bot.on_slashcommand([](const dpp::slashcommand_t& event) {
-    event.thinking(false, [event](const dpp::confirmation_callback_t &callback) {
+  bot.on_slashcommand([&config](const dpp::slashcommand_t& event) {
+    event.thinking(false, [event, &config](const dpp::confirmation_callback_t &callback) {
       if (event.command.get_command_name() == "ping") {
         event.edit_original_response(dpp::message("Pong!"));
       } else if (event.command.get_command_name() == "config") {
         dpp::command_interaction cmd_data = event.command.get_command_interaction();
-        auto featureName = cmd_data.options[0];
-        auto paramName = cmd_data.options[1];
         int guild_id{(int)(event.command.guild_id)};
-        if (featureName.name == "msgDeleteTrack") {
-          if (paramName.name == "activate") {
-            std::string val{std::get<std::string>(event.get_parameter("bool"))}
-            config.EditParam(guild_id, "msgDeleteTrack", "activate", val);
-          } else if (paramName.name == "msg") {
-            std::string val{std::get<std::string>(event.get_parameter("string"))}
-            config.EditParam(guild_id, "msgDeleteTrack", "msg", val);
-          }
-        }
+        std::string featureName{cmd_data.options[0].name};
+        std::string paramName{cmd_data.options[1].name};
+        std::string val{std::get<std::string>(event.get_parameter("val"))};
+        config.EditParam(guild_id, featureName, paramName, val);
       }
     });
   });
@@ -43,12 +36,12 @@ int main() {
       configCli.add_option(
         dpp::command_option(dpp::co_sub_command, "msgDeleteTrack", "notify deleted message")
           .add_option(dpp::command_option(dpp::co_sub_command, "activate", "true or false")
-            .add_option(dpp::command_option(dpp::co_string, "bool", "param value", true)
+            .add_option(dpp::command_option(dpp::co_string, "val", "param value", true)
               .add_choice(dpp::command_option_choice("true", std::string("true")))
               .add_choice(dpp::command_option_choice("false", std::string("false")))
             )
-          ).add_option(dpp::command_option(dp::co_sub_command, "msg", "custom message")
-            .add_option(dpp::command_option(dpp::co_string, "string", "param value", true))
+          ).add_option(dpp::command_option(dpp::co_sub_command, "msg", "custom message")
+            .add_option(dpp::command_option(dpp::co_string, "val", "param value", true))
           )
       );
 
@@ -56,7 +49,7 @@ int main() {
     }
   });
 
-  bot.on_message_delete([&bot](const dpp::message_delete_t& event) {
+  bot.on_message_delete([&bot, &config](const dpp::message_delete_t& event) {
     int guild_id{(int)(event.guild_id)};
     if ("false" == config.GetParam(guild_id, "msgDeleteTrack", "activate")) return;
     dpp::message msg(event.channel_id, config.GetParam(guild_id, "msgDeleteTrack", "msg"));
