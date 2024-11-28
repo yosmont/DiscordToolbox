1dea2aea83000d2ffcc43fb2440c88bab4937eff
yosmont <yosmont2@gmail.com>
2024-11-28 14:16:49 +0100

    restarting command logic from start

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,8 @@ void signal_handler(int sig)
   end.set_value();
 }
 
+#include <iostream>
+
 int main() {
   dotenv::env.load_dotenv();
   Config config{};
@@ -27,22 +29,54 @@ int main() {
     event.thinking(false, [event, &config](const dpp::confirmation_callback_t &callback) {
       if (event.command.get_command_name() == "ping") {
         event.edit_original_response(dpp::message("Pong!"));
-      } else if (event.command.get_command_name() == "config") {
+      /*} else if (event.command.get_command_name() == "config") {
+      	std::cout << "0" << std::endl;
         dpp::command_interaction cmd_data = event.command.get_command_interaction();
-        int guild_id{(int)(event.command.guild_id)};
-        std::string featureName{cmd_data.options[0].name};
+      	std::cout << "1" << std::endl;
+	std::string guild_id{event.command.guild_id.str()};
+      	std::cout << "2 - " << guild_id << std::endl;
+	std::cout << "3 - " << cmd_data.options.size() << std::endl;
+        //std::string featureName{cmd_data.options[0].name};
+        //std::string featureName{cmd_data.get_value<std::string>(0)};
+        std::string featureName{event.command.get_context_message().content};
+      	std::cout << "4 - " << featureName << std::endl;
+	return;
         std::string paramName{cmd_data.options[1].name};
+      	std::cout << "5 - " << paramName << std::endl;
         std::string val{std::get<std::string>(event.get_parameter("val"))};
-        config.EditParam(guild_id, featureName, paramName, val);
+      	std::cout << "6 - " << val << std::endl;
+        //config.EditParam(guild_id, featureName, paramName, val);
+      	std::cout << "7" << std::endl;
+	*/
+      } else if (event.command.get_command_name() == "config") {
+        /* Fetch a parameter value from the command parameters */
+	std::string feature = std::get<std::string>(event.get_parameter("feature"));
+	std::string param = std::get<std::string>(event.get_parameter("param"));
+	 
+	/* Reply to the command. There is an overloaded version of this
+	 * call that accepts a dpp::message so you can send embeds.
+	 */
+	event.edit_original_response(dpp::message(std::string("guild: ") + event.command.guild_id.str() + "feat: " + feature + ", param: " + param));
       }
     });
   });
 
   bot.on_ready([&bot](const dpp::ready_t& event) {
-    if (dpp::run_once<struct register_bot_commands>()) {
+    //if (dpp::run_once<struct register_bot_commands>()) {
       bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id));
 
-      dpp::slashcommand configCli("config", "Let you customize or activate feature", bot.me.id);
+      dpp::slashcommand newcommand("config", "Send a random adorable animal photo", bot.me.id);
+      newcommand.add_option(
+        dpp::command_option(dpp::co_string, "feature", "feature to configure", true)
+	  .add_choice(dpp::command_option_choice("Message deleted tracker", std::string("msgdeletetrack")))
+      ).add_option(
+	dpp::command_option(dpp::co_string, "param", "parameter to edit", true)
+	  .add_choice(dpp::command_option_choice("Activate", std::string("activate")))
+	  .add_choice(dpp::command_option_choice("Message", std::string("msg")))
+      );
+      bot.global_command_create(newcommand);
+
+      /*dpp::slashcommand configCli("config", "Let you customize or activate feature", bot.me.id);
       configCli.add_option(
         dpp::command_option(dpp::co_sub_command, "msgDeleteTrack", "notify deleted message")
           .add_option(dpp::command_option(dpp::co_sub_command, "activate", "true or false")
@@ -55,14 +89,14 @@ int main() {
           )
       );
 
-      bot.global_command_create(configCli);
-    }
+      bot.global_command_create(configCli);*/
+    //}
   });
 
   bot.on_message_delete([&bot, &config](const dpp::message_delete_t& event) {
     int guild_id{(int)(event.guild_id)};
-    if ("false" == config.GetParam(guild_id, "msgDeleteTrack", "activate")) return;
-    dpp::message msg(event.channel_id, config.GetParam(guild_id, "msgDeleteTrack", "msg"));
+    if ("false" == config.GetParam(guild_id, "msgdeletetrack", "activate")) return;
+    dpp::message msg(event.channel_id, config.GetParam(guild_id, "msgdeletetrack", "msg"));
     bot.message_create(msg);
   });
 
